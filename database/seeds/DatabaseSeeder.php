98883b11a0a724b928ec7a8cf1a1be5fe3d78954
vagrant <vagrant@homestead>
2020-03-30 08:11:16 +0000

    create auth

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTabelSeeder::class,
+            TweetsTableSeeder::class,
+            CommentsTabelSeeder::class,
+            FavoritesTableSeeder::class,
+            FollowersTableSeeder::class,
+        ]);
     }
 }
